10fae97a90168389f8b8439df4e2802e3e09c38b
kobe <a184999@gmail.com>
2015-11-10 00:11:02 +0800

    New Gravity For Android
    Has little bug need to fix

---
+++ GravityBall/Assets/BallControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+    public int speed = 10;
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb.GetComponent<Rigidbody>();
+        
+    } 
+	
+	// Update is called once per frame
+	void Update () {
+       /* if (Input.GetKeyDown(KeyCode.Space)) {
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right*10);
+        }*/
+     
+        //建立一個向量變量
+        Vector3 dir= Vector3.zero;
+         dir.z = Input.acceleration.y;
+         dir.x = Input.acceleration.x;
+         //箝制加速向量到單位球
+         if (dir.sqrMagnitude>1) {
+             dir.Normalize();
+         }
+         //每秒移動10米
+         dir *= Time.deltaTime;
+        
+         gameObject.GetComponent<Rigidbody>().AddForce(dir * speed, ForceMode.Force);
+         rb.AddForce(dir * speed, ForceMode.Force);
+
+    }
+}
---
+++ GravityBall/Assets/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    public Transform target;
+    public int relativeHeight = 10;
+    public int zDistance = 1;
+    public int dampSpeed = 1;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = target.position + new Vector3(0, relativeHeight, -zDistance);
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * dampSpeed);
+    }
+}
